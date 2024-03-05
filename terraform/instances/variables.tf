75b22ed5b0026b25fdfed392b15dcc832dc415f4
Daniel De Abreu <dlabreu@gmail.com>
2024-03-05 12:12:30 +0200

    change

--- terraform/instances/variables.tf
+++ terraform/instances/variables.tf
@@ -1,28 +1,7 @@
-variable "key_name" {
-  description = "The name of the SSH key pair to use for the EC2 instances"
-}
-
-variable "security_group_ids" {
-  description = "A list of security group IDs to associate with the EC2 instances"
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC where the EC2 instances will be launched"
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet where the EC2 instances will be launched"
-}
-
-variable "instance_count" {
-  description = "The number of EC2 instances to launch"
-  type        = number
-}
-
-variable "instance_type" {
-  description = "The type of EC2 instance to launch"
-}
-
-variable "ami_id" {
-  description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instances"
-}
+variable key_name {}
+variable security_group_ids {}
+variable vpc_id {}
+variable subnet_id {}
+variable instance_count {}
+variable instance_type {}
+variable ami_id {}
