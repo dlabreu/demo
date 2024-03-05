934e9c3cebfbccb98a0f74a2c83634de33b062db
Daniel De Abreu <dlabreu@gmail.com>
2024-03-05 11:38:54 +0200

    Add new code

--- terraform/instances/variables.tf
+++ terraform/instances/variables.tf
@@ -1,5 +1,13 @@
+variable "key_name" {}
+variable "security_group_ids" {}
 variable "vpc_id" {}
 variable "subnet_id" {}
 variable "instance_count" {}
 variable "instance_type" {}
 variable "ami_id" {}
+variable "tags" {
+  type = "map"
+  default = {
+    Name = "MyInstance"
+    Environment = "Dev"}
+}
