f479b9b0062979726dc27b2b092f2f68d8fc6803
Daniel De Abreu <dlabreu@gmail.com>
2024-03-05 13:45:35 +0200

    Update vpc.tf

--- terraform/network/vpc.tf
+++ terraform/network/vpc.tf
@@ -29,38 +29,3 @@ resource "aws_main_route_table_association" "a" {
   vpc_id         = aws_vpc.main.id
   route_table_id = aws_route_table.r.id
 }
-
-resource "aws_security_group" "my_security_group" {
-  name        = "app-security-group"
-  description = "Allow SSH, Cockpit, and HTTP traffic"
-
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (Please update as per your security requirements)
-  }
-
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow Cockpit from anywhere (Please update as per your security requirements)
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere (Please update as per your security requirements)
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
