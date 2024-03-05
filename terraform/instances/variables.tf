655c9980715568d8b31d0c6a62945d9417a83ae3
Daniel De Abreu <dlabreu@gmail.com>
2024-03-05 12:25:33 +0200

    remove all stuffs of the way

--- terraform/instances/variables.tf
+++ terraform/instances/variables.tf
@@ -1,6 +1,12 @@
-variable key_name {}
-variable vpc_id {}
-variable subnet_id {}
-variable instance_count {}
-variable instance_type {}
-variable ami_id {}
+variable key_name {
+    default = "id_rsa.pub"
+}
+
+variable security_group_ids {
+    default = "sg-09b73fd8672058b75"
+}
+variable vpc_id : "vpc-09d79c26d86172a1a"
+variable subnet_id: "subnet-04837fbe02da83c4d"
+variable instance_count: "2"
+variable instance_type: "t2.micro"
+variable ami_id: "ami-0134dde2b68fe1b07"
